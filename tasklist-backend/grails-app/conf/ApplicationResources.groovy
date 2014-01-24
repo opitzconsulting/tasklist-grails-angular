028636aa57e9d18c20590f9165d4f19f058d6df7
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2014-01-24 16:12:02 +0100

    split tasks.js into multiple files

--- tasklist-backend/grails-app/conf/ApplicationResources.groovy
+++ tasklist-backend/grails-app/conf/ApplicationResources.groovy
@@ -18,6 +18,8 @@ modules = {
         dependsOn 'angularjs'
         dependsOn 'bootstap'
         resource url: 'js/tasks.js'
+        resource url: 'js/tasks_controller.js'
+        resource url: 'js/tasks_resource.js'
         resource url: 'css/tasks.css'
     }
 
