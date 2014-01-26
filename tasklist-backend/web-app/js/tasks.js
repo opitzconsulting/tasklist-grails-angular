bc6d8aa8227ddb70f1b860469bf16f385962a637
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2014-01-26 16:31:02 +0100

    Simplification by putting all Angular JS stuff in one file.

--- tasklist-backend/web-app/js/tasks.js
+++ tasklist-backend/web-app/js/tasks.js
@@ -1,10 +1,48 @@
-angular.module("app", ["ngResource"]).run(function ($rootScope) {
-    // adds some basic utilities to the $rootScope for debugging purposes
-    $rootScope.log = function (thing) {
-        console.log(thing);
+angular.module("app", ["ngResource"]);
+
+
+angular.module("app").factory("TaskResource", function ($q, $resource) {
+
+    return $resource('/api/tasks/:id', null, {
+        'update': { method: 'PUT' }
+    });
+
+});
+
+
+angular.module("app").controller("TaskCtrl", function ($scope, TaskResource) {
+
+    $scope.tasks = TaskResource.query();
+
+    $scope.addTask = function () {
+        var newTask = new TaskResource({title: $scope.taskTitle, done: false});
+        newTask.$save(function (savedTask) {
+            $scope.tasks.push(savedTask);
+        });
+        $scope.taskTitle = '';
     };
 
-    $rootScope.alert = function (thing) {
-        alert(thing);
+    $scope.updateTask = function(task) {
+        task.done = !task.done;
+        task.$update();
     };
+
+    $scope.remaining = function () {
+        var count = 0;
+        angular.forEach($scope.tasks, function (task) {
+            count += task.done ? 0 : 1;
+        });
+        return count;
+    };
+
+    $scope.archive = function () {
+        var oldTasks = $scope.tasks;
+        $scope.tasks = [];
+        angular.forEach(oldTasks, function (task) {
+            if (!task.done) {
+                $scope.tasks.push(task);
+            }
+        });
+    };
+
 });
