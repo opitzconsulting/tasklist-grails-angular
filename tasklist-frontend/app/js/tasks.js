ad5f432122f58f15b16e1a3690e6870cf8679543
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2014-11-20 14:37:49 +0100

    demo-baseline-part-3

--- tasklist-frontend/app/js/tasks.js
+++ tasklist-frontend/app/js/tasks.js
@@ -2,14 +2,48 @@
     'use strict';
 
     var module = angular.module('tasklist.tasks', [
-        'ngRoute'
+        'ngRoute',
+        'ngStorage'
     ]);
 
     module.config(function ($routeProvider) {
         $routeProvider
             .when('/tasks', {
-                templateUrl: 'tasks.html'
+                templateUrl: 'tasks.html',
+                controller: 'TaskListCtrl'
             });
+    });
+
+    module.factory('TaskRepository', function ($localStorage) {
+        var storage = $localStorage.$default({tasks: [], initialized: false});
+        if (!storage.initialized) {
+            storage.tasks.push({title: 'One', done: false});
+            storage.tasks.push({title: 'Two', done: false});
+            storage.tasks.push({title: 'Three', done: false});
+            storage.initialized = true;
+        }
+        return {
+            allTasks: function () {
+                return storage.tasks;
+            },
+            addTask: function (task) {
+                storage.tasks.push(task);
+            },
+            deleteTaskAtIndex: function (index) {
+                storage.tasks.splice(index, 1);
+            }
+        };
+    });
+
+    module.controller('TaskListCtrl', function ($scope, TaskRepository) {
+        $scope.tasks = TaskRepository.allTasks();
+        $scope.addTask = function () {
+            TaskRepository.addTask({title: $scope.newTaskTitle, done: false});
+            $scope.newTaskTitle = '';
+        };
+        $scope.deleteTask = function (index) {
+            TaskRepository.deleteTaskAtIndex(index);
+        };
     });
 
 })(window.angular);
--- tasklist-frontend/spec/tasks/tasks_spec.js
+++ tasklist-frontend/spec/tasks/tasks_spec.js
@@ -2,4 +2,16 @@ describe('tasklist.tasks', function () {
 
     beforeEach(module('tasklist.tasks'));
 
+    describe('TaskListCtrl', function () {
+        it('loads all tasks', inject(function ($controller, TaskRepository) {
+            var scope = {};
+            var allTasks = [];
+            spyOn(TaskRepository, 'allTasks').andReturn(allTasks);
+
+            $controller('TaskListCtrl', {$scope: scope, TaskRepository: TaskRepository});
+
+            expect(scope.tasks).toBe(allTasks);
+        }));
+    });
+
 });
