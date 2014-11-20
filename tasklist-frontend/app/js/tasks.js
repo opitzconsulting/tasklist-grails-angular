36689b5b3d259aad43927b74c8216b7d1d796aea
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2014-11-20 13:45:22 +0100

    demo-baseline: Frontend Baseline

--- tasklist-frontend/app/js/tasks.js
+++ tasklist-frontend/app/js/tasks.js
@@ -2,42 +2,14 @@
     'use strict';
 
     var module = angular.module('tasklist.tasks', [
-        'ngRoute',
-        'ngResource'
+        'ngRoute'
     ]);
 
     module.config(function ($routeProvider) {
         $routeProvider
             .when('/tasks', {
-                templateUrl: 'tasks.html',
-                controller: 'TaskListCtrl'
+                templateUrl: 'tasks.html'
             });
-    });
-
-    module.factory("TaskResource", function ($q, $resource) {
-        return $resource('/tasklist-backend/tasks/:id', { id: '@id' }, {
-            'update': { method: 'PUT' }
-        });
-    });
-
-    module.controller('TaskListCtrl', function ($scope, TaskResource) {
-        $scope.tasks = TaskResource.query();
-        $scope.addTask = function () {
-            var newTask = new TaskResource({title: $scope.newTaskTitle, done: false});
-            newTask.$save(function (savedTask) {
-                $scope.tasks.push(savedTask);
-                $scope.newTaskTitle = '';
-            });
-        };
-        $scope.checkTask = function(task) {
-            task.done = !task.done;
-            task.$update();
-        };
-        $scope.deleteTask = function (index) {
-            $scope.tasks[index].$delete(function() {
-                $scope.tasks.splice(index, 1);
-            });
-        };
     });
 
 })(window.angular);
