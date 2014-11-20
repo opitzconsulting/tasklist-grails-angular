c82994c26fe7676725993166ad5338672f280978
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2014-11-20 14:11:28 +0100

    demo-baseline: cleanup

--- tasklist-frontend/spec/tasks/tasks_spec.js
+++ tasklist-frontend/spec/tasks/tasks_spec.js
@@ -2,16 +2,4 @@ describe('tasklist.tasks', function () {
 
     beforeEach(module('tasklist.tasks'));
 
-    describe('TaskListCtrl', function () {
-        it('loads all tasks', inject(function ($controller, TaskResource) {
-            var scope = {};
-            var allTasks = [];
-            spyOn(TaskResource, 'query').andReturn(allTasks);
-
-            $controller('TaskListCtrl', {$scope: scope, TaskResource: TaskResource});
-
-            expect(scope.tasks).toBe(allTasks);
-        }));
-    });
-
 });
